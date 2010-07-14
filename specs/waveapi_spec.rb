ccd96a717b8599ee7b32fee4f453bb05ebb67c52
ANDO Yasushi <andyjpn@gmail.com>
2010-07-15 00:46:17 +0900

    add spec

--- specs/waveapi_spec.rb
+++ specs/waveapi_spec.rb
@@ -18,6 +18,21 @@ require 'waveapi'
 =end
 
 describe Waveapi do
+  it 'blip.append(Image)' do 
+    robot = Waveapi::Robot.new('Test Robot')
+    robot.register_handler(Waveapi::BlipSubmittedEvent) do |event, wavelet|
+      event.blip.append(Waveapi::Image.new('http://www.google.com/logos/clickortreat1.gif', 320, 118))
+    end
+
+    incoming_json = '{"events":[{"type":"BLIP_SUBMITTED","modifiedBy":"andyjpn@googlewave.com","timestamp":1279122274210,"properties":{"blipId":"b+Mg1By4lSD"}}],"wavelet":{"creationTime":1279113360748,"lastModifiedTime":1279122274210,"version":48,"participants":["andyjpn@googlewave.com","ruby-teacher.on-wave@appspot.com"],"participantRoles":{"ruby-teacher.on-wave@appspot.com":"FULL","andyjpn@googlewave.com":"FULL"},"dataDocuments":{},"tags":[],"creator":"andyjpn@googlewave.com","rootBlipId":"b+Mg1By4lSD","title":"A wavelet title","waveId":"googlewave.com!w+Mg1By4lSC","waveletId":"googlewave.com!conv+root","rootThread":null},"blips":{"b+Mg1By4lSD":{"annotations":[{"name":"conv/title","value":"","range":{"start":0,"end":16}},{"name":"lang","value":"en","range":{"start":0,"end":16}}],"elements":{"0":{"type":"LINE","properties":{}},"16":{"type":"LINE","properties":{}}},"blipId":"b+Mg1By4lSD","childBlipIds":[],"contributors":["andyjpn@googlewave.com","ruby-teacher.on-wave@appspot.com"],"creator":"andyjpn@googlewave.com","content":"\nA wavelet title\n","lastModifiedTime":1279119087694,"parentBlipId":null,"version":42,"waveId":"googlewave.com!w+Mg1By4lSC","waveletId":"googlewave.com!conv+root","replyThreadIds":[],"threadId":null}},"threads":{},"robotAddress":"ruby-teacher.on-wave@appspot.com"}'
+
+    outgoing_json = JSON.parse('[{"params": {"capabilitiesHash": "", "protocolVersion": "0.21"}, "method": "robot.notifyCapabilitiesHash", "id": "0"}, {"params": {"blipId": "b+Mg1By4lSD", "waveletId": "googlewave.com!conv+root", "waveId": "googlewave.com!w+Mg1By4lSC", "modifyAction": {"modifyHow": "INSERT_AFTER", "elements": [{"type": "IMAGE", "properties": {"url": "http://www.google.com/logos/clickortreat1.gif", "width": 320, "height": 118}}]}}, "method": "document.modify", "id": "op1"}]')
+
+    result_json = JSON.parse(robot.handle(incoming_json))
+    result_json.first['params']['capabilitiesHash'] = ''
+    result_json.should eql(outgoing_json)
+  end
+
   it 'wavelet.title=' do 
     robot = Waveapi::Robot.new('Test Robot')
     robot.register_handler(Waveapi::BlipSubmittedEvent) do |event, wavelet|
